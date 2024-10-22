28e869571bfaf05a874ba7eeab9559d2af36b4ce
Shooq Janahi <121062980+ShooqJanahi@users.noreply.github.com>
2024-10-22 18:40:53 +0300

    Revert "admin dashboard"
    This reverts commit 5a7c6835164c6b30227621db98dd50a51b9ee48c.

--- js/AdminDashboard.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('settings').addEventListener('click', function() {
-    alert('Settings clicked');
-});
-
-document.getElementById('logout').addEventListener('click', function() {
-    alert('Logout clicked');
-});
