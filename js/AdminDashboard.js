8e293a5ac0aeeb7259a1a54cd627225060513e51
Shooq Janahi <121062980+ShooqJanahi@users.noreply.github.com>
2024-10-23 15:44:34 +0300

    created admin dashboard

---
+++ js/AdminDashboard.js
@@ -0,0 +1,7 @@
+document.getElementById('settings').addEventListener('click', function() {
+    alert('Settings clicked');
+});
+
+document.getElementById('logout').addEventListener('click', function() {
+    alert('Logout clicked');
+});
