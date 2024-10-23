1556ac38d7581e9bc79aa97e5815bbed2251a665
Shooq Janahi <121062980+ShooqJanahi@users.noreply.github.com>
2024-10-23 22:30:07 +0300

    the Admin dashboard is now responsove

--- js/AdminDashboard.js
+++ js/AdminDashboard.js
@@ -5,3 +5,8 @@ document.getElementById('settings').addEventListener('click', function() {
 document.getElementById('logout').addEventListener('click', function() {
     alert('Logout clicked');
 });
+
+
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.mobile-menu').classList.toggle('show');
+});
